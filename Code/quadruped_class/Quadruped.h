6ee978e7030e069f641b2f4488c5e4debd38dc68
Gabriel <gabrieltvantuil@gmail.com>
2018-06-15 20:18:06 -0300

    working

---
+++ Code/quadruped_class/Quadruped.h
@@ -0,0 +1,44 @@
+#include "Arduino.h"
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+
+class Quadruped{
+    private:
+        int speed;
+        int DOFs_leg;
+        Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+        int teste;
+        int resolution;
+        int ampA;
+        int ampB; 
+        int ampG;
+        float phase[4];
+        int Xa, Xb, Xc, Xd, Xe, Xf, Xg;
+        float alpha, beta, gamma;
+        bool GC_correct=0;
+        
+    public:
+        Quadruped(int DOF);
+        int altura[4] = {0,0,0,0};
+        void real_pos(char pos[]);
+        void walk_pos(char pos[]);
+        void plot_curves(int l);
+        void walk(float dirD, float dirE);
+        void setMotorAngle(uint8_t num, uint16_t degree);
+        void mover(char pos[],float dirE, float dirD);
+        void begin();
+//////////////////////////////////////////////////////
+        void walk_leg(int pata);
+        void set_curve_parameters(int xa, int xb, int xc, int xd, int xe, int xf, int xg);
+        void set_phase(float Phase[]);
+        void set_amp_alpha(int Alpha){ampA =Alpha;}
+        void set_amp_beta(int Beta){ampB = Beta;}
+        void set_amp_gamma(int Gamma){ampG = Gamma;}
+        void set_speed(int s){speed=s;}
+        void set_resolution(int c){resolution=c;}
+        int get_alpha(){return (int)alpha;}
+        int get_beta(){return (int)beta;}
+        int get_gamma(){return (int)gamma;}
+
+
+};
